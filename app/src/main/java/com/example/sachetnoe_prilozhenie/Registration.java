eb8955f1eb8b938edd381cab747bb21c6189417a
Vadim <108000499+Capybarin22@users.noreply.github.com>
2023-06-17 21:22:58 +0300

    Update comments.java

--- app/src/main/java/com/example/sachetnoe_prilozhenie/Registration.java
+++ app/src/main/java/com/example/sachetnoe_prilozhenie/Registration.java
@@ -13,8 +13,10 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
+// Создание класса для регистрации пользователя
 public class Registration extends AppCompatActivity {
 
+    // Создание переменных для забора значений из полей ввода
     private EditText fioEditText, emailEditText, passwordEditText;
     private CheckBox statusCheckBox;
     private Switch PolSwitch;
@@ -28,31 +30,32 @@ public class Registration extends AppCompatActivity {
 
         dbHelper = new DatabaseHelper_Users_Merop(this);
 
+        // Получаем соответствующие формы ввода из XML 
         fioEditText = findViewById(R.id.org);
         emailEditText = findViewById(R.id.fioText);
         passwordEditText = findViewById(R.id.password);
         statusCheckBox = findViewById(R.id.statusCheckBox);
         PolSwitch = findViewById(R.id.polSwitch);
 
-        // Устанавливаем мужской пол по умолчанию
+        // Мужской пол по умолчанию
         PolSwitch.setChecked(false);
     }
 
     public void savereg(View view) {
-        // Получаем значения полей со страницы регистрации
+       // Забор значений из полей для ввода
         String fio = fioEditText.getText().toString().trim();
         String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
         String status = "";
         int reating = 0;
-        //здесь был Гиша
-        //Capybarin was here
 
+        // Проверка заполненности чекбокса для присвоения статуса регистрируемому 
         if (statusCheckBox.isChecked()) {
             status = "Организатор";
         } else {
             status = "Волонтёр";
         }
+        // Проверка тоггл-кнопки для присвоения пола регистрируемому
         String pol = "";
         if (PolSwitch.isChecked()) {
             pol = "Женский";
@@ -60,7 +63,7 @@ public class Registration extends AppCompatActivity {
             pol = "Мужской";
         }
 
-        // Проверяем, что все поля заполнены
+        // Проверяем, что все текстовые поля заполнены
         if (!fio.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
             SQLiteDatabase db = dbHelper.getWritableDatabase();
             ContentValues values = new ContentValues();
@@ -71,33 +74,38 @@ public class Registration extends AppCompatActivity {
             values.put(DatabaseHelper_Users_Merop.COLUMN_STATUS, status);
             values.put(DatabaseHelper_Users_Merop.COLUMN_REATING, reating);
 
-            // Проверяем наличие пользователя с указанным email
+            // Проверяем уникальность email
             boolean exists = dbHelper.checkUser(email);
             while (exists) {
-                // Этот email уже занят, запрашиваем у пользователя новый email
+                // Если этот email уже занят, запрашиваем у пользователя новый email
                 Toast.makeText(this, "Пользователь с таким email уже существует", Toast.LENGTH_SHORT).show();
-                emailEditText.requestFocus(); // устанавливаем фокус на поле ввода email
-                return; // выходим из метода и ждем, пока пользователь введет уникальный email
+                emailEditText.requestFocus(); // Устанавливаем фокус на поле ввода email
+                return; // Выходим из метода и ждем, пока пользователь введет уникальный email
             }
 
             // Добавляем нового пользователя в базу данных
             boolean success = dbHelper.insertData(email, password, fio, status, pol, reating);
+            // Если регистрация прошла успешно, уведомляем об этом пользователя и переводим его в личный кабинет 
             if (success) {
                 Toast.makeText(this, "Пользователь зарегистрирован", Toast.LENGTH_SHORT).show();
+                
                 // Переходим на экран личного кабинета
                 Intent intent = new Intent(this, Autor_Regis.class);
                 intent.putExtra("email", email);
                 startActivity(intent);
                 finish();
+            
+            // Иначе, уведомляем о неудаче 
             } else {
                 Toast.makeText(this, "Ошибка при регистрации пользователя", Toast.LENGTH_SHORT).show();
             }
+            // Просим регистрируемого дозаполнить поля
         } else {
             Toast.makeText(this, "Пожалуйста, заполните все поля", Toast.LENGTH_SHORT).show();
         }
     }
 
-    // закрываем базу данных при уничтожении активности
+    // Закрываем базу данных при уничтожении активности
     @Override
     protected void onDestroy() {
         super.onDestroy();
