3f1c2ab2b4e373bd21552bb6aca1fbbab64da734
Vadim <108000499+Capybarin22@users.noreply.github.com>
2023-06-18 14:47:02 +0300

    Update Main_Menu.java

--- app/src/main/java/com/example/sachetnoe_prilozhenie/Main_Menu.java
+++ app/src/main/java/com/example/sachetnoe_prilozhenie/Main_Menu.java
@@ -19,106 +19,101 @@ public class Main_Menu extends AppCompatActivity {
     String fio;
     public int userId;
 
-    @SuppressLint("MissingInflatedId")
-    @Override
+    // Создание активности страницы главного меню
+    @SuppressLint("MissingInflatedId") // Заглушка для предупреждения android-studio
+    @Override // Переопределение родительского метода
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu);
+        super.onCreate(savedInstanceState); // Создание активности
+        setContentView(R.layout.main_menu); // Устанавливаем макет пользовательского интерфейса "main_menu" для этой активности
 
-        textEmail = findViewById(R.id.EmailText);
-        ButtonControl = findViewById(R.id.button16);
+        textEmail = findViewById(R.id.EmailText); // Связываем  переменную логина с текстовым полем для ввода логина
+        ButtonControl = findViewById(R.id.button16); // Связываем переменную кнопки с кнопкой для управления мероприятиями
 
-        meropList = findViewById(R.id.list);
+        meropList = findViewById(R.id.list); // Связываем переменную листа мероприятий со списком мероприятий
 
-        Intent intent0 = getIntent();
-        String email = intent0.getStringExtra("email");
-        status = intent0.getStringExtra("status");
-        fio = intent0.getStringExtra("fio");
-        userId = getIntent().getIntExtra("id", -2);
-        int[] colors = new int[]{
-                Color.parseColor("#202630"),
-                Color.parseColor("#313741"),};
+        Intent intent0 = getIntent(); // Создаём объект для передачи данных между классами
+        String email = intent0.getStringExtra("email"); // Получаем значение логина
+        status = intent0.getStringExtra("status"); // Получаем значение статуса
+        fio = intent0.getStringExtra("fio"); // Получаем значение ФИО
+        userId = getIntent().getIntExtra("id", -2); // Получаем значение уникального идентификатора пользователя, если значение не найдено, возвращаем "-2"
+        int[] colors = new int[]{ // Создаём массив из двух цветов(для чередования цветов списка доступных мероприятий)
+                Color.parseColor("#202630"), // Тёмно-серый
+                Color.parseColor("#202630"), // Светло-серый
+        };
 
-        textEmail.setText(email);
+        textEmail.setText(email); // Значение переменной "email" устанавливается для текстового поля логина.
 
-        if (status != null && status.equals("Статус: Волонтёр")) {
-            ButtonControl.setVisibility(View.GONE); // скрыть кнопку
-        } else {
-            ButtonControl.setVisibility(View.VISIBLE); // показать кнопку
+        if (status != null && status.equals("Статус: Волонтёр")) { // Если статус пользователя = волонтер
+            ButtonControl.setVisibility(View.GONE); // Скрываем кнопку управления мероприятиями
+        } else { // Если статус пользователя = организатор
+            ButtonControl.setVisibility(View.VISIBLE); // Отображаем кнопку
         }
 
-        meropList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int meropId = (int) id;
+        meropList.setOnItemClickListener(new AdapterView.OnItemClickListener() { // Создаём слушатель событий нажатий на элементы списка мероприятий
+            @Override  // Переопределение родительского метода
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) { // Метод, который вызызывается по нажатию на элемент списка
+                int meropId = (int) id; // Создаем переменную под id мероприятия
 
-                // изменяем цвет фона выбранного мероприятия
-                view.setBackgroundColor(colors[position % colors.length]);
+                view.setBackgroundColor(colors[position % colors.length]); // Изменяем цвет фона выбранного мероприятия
 
-                // остальной код обработки нажатия на мероприятие
                 String query = "SELECT * FROM " + DatabaseHelper_Users_Merop.TABLE_Z + " WHERE " +
                         DatabaseHelper_Users_Merop.COLUMN_ID_USER_Z + "=" + userId + " AND " +
-                        DatabaseHelper_Users_Merop.COLUMN_ID_MER_Z + "=" + meropId;
-                SQLiteDatabase db = databaseHelper.getWritableDatabase();
-                Cursor cursor = null;
-                try {
-                    cursor = db.rawQuery(query, null);
-                    if (cursor.getCount() > 0) {
-                        Toast.makeText(Main_Menu.this, "Вы уже зарегистрированы на это мероприятие", Toast.LENGTH_SHORT).show();
-                    } else {
-                        ContentValues contentValues = new ContentValues();
-                        contentValues.put(DatabaseHelper_Users_Merop.COLUMN_ID_USER_Z, userId);
-                        contentValues.put(DatabaseHelper_Users_Merop.COLUMN_ID_MER_Z, meropId);
-                        long result = db.insert(DatabaseHelper_Users_Merop.TABLE_Z, null, contentValues);
-                        if (result == -1) {
-                            Toast.makeText(Main_Menu.this, "Ошибка при сохранении данных", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(Main_Menu.this, "Заявка на мероприятие сохранена", Toast.LENGTH_SHORT).show();
+                        DatabaseHelper_Users_Merop.COLUMN_ID_MER_Z + "=" + meropId; // Переменная для запроса к БД
+                SQLiteDatabase db = databaseHelper.getWritableDatabase(); // Получаем экземпляр с в режиме "для изменения"
+                Cursor cursor = null; // Создаем объект для получения доступа к результатам запроса в базе данных
+                try { // Блок кода для сохранения пользовательской заявки на мероприятие в БД
+                    cursor = db.rawQuery(query, null); //Получаем данные от запроса к БД 
+                    if (cursor.getCount() > 0) { // Проверяем наличие записей в БД, если такая запиь уже есть, то
+                        Toast.makeText(Main_Menu.this, "Вы уже зарегистрированы на это мероприятие", Toast.LENGTH_SHORT).show(); // Выводим сообщение о статусе регистрации волонтера
+                    } else { // Иначе
+                        ContentValues contentValues = new ContentValues(); // Создаем новую запись в базе данных
+                        contentValues.put(DatabaseHelper_Users_Merop.COLUMN_ID_USER_Z, userId); // Добавляем значение пользовательного идентификатора в соответствующий столбец в таблице БД
+                        contentValues.put(DatabaseHelper_Users_Merop.COLUMN_ID_MER_Z, meropId); // Добавляем  значение идентификатора мероприятий в соответствующий столбец в таблице БД
+                        long result = db.insert(DatabaseHelper_Users_Merop.TABLE_Z, null, contentValues); // Добавляем полученную запись в БД
+                        if (result == -1) { // Если данные в БД не были сохранены
+                            Toast.makeText(Main_Menu.this, "Ошибка при сохранении данных", Toast.LENGTH_SHORT).show(); // Выводим сообщение об ошибке
+                        } else { // Иначе
+                            Toast.makeText(Main_Menu.this, "Заявка на мероприятие сохранена", Toast.LENGTH_SHORT).show(); // Выводим сообщение об успешном выполнении добавления мероприятия
                         }
                     }
-                } finally {
-                    if (cursor != null) {
-                        cursor.close();
+                } finally { // Независимо от успешности операции добавления мероприятия в БД
+                    if (cursor != null) { // Проверка открытости курсора
+                        cursor.close(); // Если открыт - закрываем
                     }
-                    db.close();
+                    db.close(); // Закрытие соединения с базой данных для освобождения ресурсов и избежания потенциальных проблем с безопасностью
                 }
             }
         });
 
 
-        databaseHelper = new DatabaseHelper_Users_Merop(getApplicationContext());
-        db = databaseHelper.getReadableDatabase(); // инициализируем базу данных в onCreate()
+        databaseHelper = new DatabaseHelper_Users_Merop(getApplicationContext());  // Создание экземпляра класса БД для этой активности
+        db = databaseHelper.getReadableDatabase(); // Открываем БД в режиме "только чтения"
     }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    @Override
+    
+     // Если приложение вернется к этой активности из другой, например, когда пользователь нажмет на кнопку "назад"
+    @RequiresApi(api = Build.VERSION_CODES.O) // Приложение не будет выполняться на устройствах с более старыми версиями Android, которые не поддерживают следующие функции
+    @Override // Переопределение родительского метода
     public void onResume() {
-        super.onResume();
+        super.onResume();  // Возобновляем активность
 
-// закрываем курсор, если он не закрыт
-        if (userCursor != null && !userCursor.isClosed()) {
-            userCursor.close();
+        if (userCursor != null && !userCursor.isClosed()) { // Проверка открытости курсора
+            userCursor.close(); // Если открыт - закрываем
         }
 
-//открываем базу данных
-        db = databaseHelper.getReadableDatabase();
+        db = databaseHelper.getReadableDatabase(); // Открываем БД в режиме "только чтения"
 
-//получаем данные из бд в виде курсора
-        userCursor = db.rawQuery("select * from " + DatabaseHelper_Users_Merop.TABLE_M, null);
-// определяем, какие столбцы из курсора будут выводиться в ListView
-        String[] headers = new String[]{DatabaseHelper_Users_Merop.COLUMN_NAME, DatabaseHelper_Users_Merop.COLUMN_VREMA, DatabaseHelper_Users_Merop.COLUMN_ORG, DatabaseHelper_Users_Merop.COLUMN_OPISANIE};
-// создаем адаптер, передаем в него курсор
-        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
-                userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
-        meropList.setAdapter(userAdapter);
-// сбрасываем цвета фонов мероприятий
-        for (int i = 0; i < meropList.getChildCount(); i++) {
-            View child = meropList.getChildAt(i);
-            child.setBackgroundColor(Color.TRANSPARENT);
+        userCursor = db.rawQuery("select * from " + DatabaseHelper_Users_Merop.TABLE_M, null);  // Записываем в переменную курсора все строки таблицы "DatabaseHelper_Users_Merop.TABLE_M"
+        String[] headers = new String[]{DatabaseHelper_Users_Merop.COLUMN_NAME, DatabaseHelper_Users_Merop.COLUMN_VREMA, DatabaseHelper_Users_Merop.COLUMN_ORG, DatabaseHelper_Users_Merop.COLUMN_OPISANIE}; // Выбираем из БД столбцы , которые будут выводиться в ListView
+        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item, // Связываем результаты запроса к базе данных с пользовательским интерфейсом
+                userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0); // Обозначаем массив названий столбцов, в которых нужно отображать данные, и массив ресурсов пользовательского интерфейса
+        meropList.setAdapter(userAdapter); // Устанавливаем адаптер userAdapter для списка meropList
+        for (int i = 0; i < meropList.getChildCount(); i++) { // Создаём цикл, который перебирает все элементы в списке мероприятий
+            View child = meropList.getChildAt(i); // Получаем элементы списка для дальнейшего взаимодействия с ними
+            child.setBackgroundColor(Color.TRANSPARENT); // Устанавливаем фон элементов списка мероприятий
         }
     }
 
-    // добавляем метод для кнопки "На главную"
+    // Создаём метод для кнопки "На главную"
     public void home(View view) {
         Intent intent1 = new Intent(this, My_World.class);
         intent1.putExtra("email", textEmail.getText().toString());
@@ -128,7 +123,7 @@ public class Main_Menu extends AppCompatActivity {
         startActivity(intent1);
     }
 
-    // добавляем метод для кнопки "Управление"
+    // Создаём метод для кнопки "Управление"
     public void use(View view) {
         Intent intent1 = new Intent(this, Upravlat.class);
         intent1.putExtra("email", textEmail.getText().toString());
@@ -138,7 +133,7 @@ public class Main_Menu extends AppCompatActivity {
         startActivity(intent1);
     }
 
-    @Override
+    @Override // Переопределение родительского метода
     public void onDestroy() {
         super.onDestroy();
         // Закрываем подключение и курсор
