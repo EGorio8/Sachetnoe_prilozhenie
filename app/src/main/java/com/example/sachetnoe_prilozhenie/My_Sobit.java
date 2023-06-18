5fe171c8101cd2c72f419a8815c9d6002fc97a52
Vadim <108000499+Capybarin22@users.noreply.github.com>
2023-06-18 15:41:59 +0300

    Update and rename UserEventsScreen.java to My_Sobit.java

--- app/src/main/java/com/example/sachetnoe_prilozhenie/My_Sobit.java
+++ app/src/main/java/com/example/sachetnoe_prilozhenie/My_Sobit.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class UserEventsScreen extends AppCompatActivity {
+public class My_Sobit extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
