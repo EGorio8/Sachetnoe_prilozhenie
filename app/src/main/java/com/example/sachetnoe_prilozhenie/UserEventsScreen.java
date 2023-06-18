db894903536652aab2994025ed631c0fe2539616
Vadim <108000499+Capybarin22@users.noreply.github.com>
2023-06-18 15:22:25 +0300

    Update UserEventsScreen.java

--- app/src/main/java/com/example/sachetnoe_prilozhenie/UserEventsScreen.java
+++ app/src/main/java/com/example/sachetnoe_prilozhenie/UserEventsScreen.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class My_Sobit extends AppCompatActivity {
+public class UserEventsScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
