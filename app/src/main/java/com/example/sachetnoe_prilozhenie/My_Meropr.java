f23de3e8f365b8477eccdaac2a906ced4ba9720a
Vadim <108000499+Capybarin22@users.noreply.github.com>
2023-06-18 15:50:40 +0300

    Update My_Meropr.java

--- app/src/main/java/com/example/sachetnoe_prilozhenie/My_Meropr.java
+++ app/src/main/java/com/example/sachetnoe_prilozhenie/My_Meropr.java
@@ -9,13 +9,13 @@ import android.widget.Button;
 
 public class My_Meropr extends AppCompatActivity {
 
-    @Override
+    @Override // Переопределение родительского метода
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_meropr);
+        super.onCreate(savedInstanceState); // Создание активности
+        setContentView(R.layout.my_meropr); // Устанавливаем макет пользовательского интерфейса "my_meropr" для этой активности
     }
-
-    public void onBackPressed(View view) {
-        super.onBackPressed();
+   
+    public void onBackPressed(View view) {  // Создаём метод для обработки события нажатия кнопки возвращения на предыдущую активность
+        super.onBackPressed(); // Возвращаемся к предыдущей активности
     }
 }
