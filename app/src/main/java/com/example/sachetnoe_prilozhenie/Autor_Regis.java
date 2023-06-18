bf02af861c279dffa950cf5e60f036a118a5c7a2
Vadim <108000499+Capybarin22@users.noreply.github.com>
2023-06-18 11:31:32 +0300

    Update Autor_Regis.java

--- app/src/main/java/com/example/sachetnoe_prilozhenie/Autor_Regis.java
+++ app/src/main/java/com/example/sachetnoe_prilozhenie/Autor_Regis.java
@@ -13,43 +13,44 @@ import android.widget.Toast;
 // Класс для авторизации пользователей
 public class Autor_Regis extends AppCompatActivity {
 
-    private EditText emailEditText, passwordEditText;
-    private DatabaseHelper_Users_Merop dbHelper;
+    private EditText emailEditText, passwordEditText; // Создание переменных под логин и пароль пользователя
+    private DatabaseHelper_Users_Merop dbHelper; // Создание переменной под БД
 
+    // Создание активности страницы авторизации / регистрации пользователя
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.autor_regis);
+        super.onCreate(savedInstanceState); // Создание активности
+        setContentView(R.layout.autor_regis); // Устанавливаем макет пользовательского интерфейса "autor_regis" для этой активности
 
-        dbHelper = new DatabaseHelper_Users_Merop(this); // Создание экземпляра класса DatabaseHelper
+        dbHelper = new DatabaseHelper_Users_Merop(this); // Создание экземпляра класса БД для этой активности
 
-        emailEditText = findViewById(R.id.fioText);
-        passwordEditText = findViewById(R.id.password);
+        emailEditText = findViewById(R.id.fioText); // Связываем переменную логина с текстовым полем для ввода логина
+        passwordEditText = findViewById(R.id.password); // Связываем переменную пароля с текстовым полем для ввода пароля
     }
 
-    // Метод для выполнения входа в приложение
+    // Метод для выполнения входа в приложение(код, выполняющийся по нажатию на кнопку "Войти")
     public void enter(View view) {
 
-        String email = emailEditText.getText().toString().trim();
-        String password = passwordEditText.getText().toString().trim();
+        String email = emailEditText.getText().toString().trim(); // Получаем значение электронной почты пользователя из соответствующего текстового поля
+        String password = passwordEditText.getText().toString().trim(); // Получаем значение пароля пользователя из соответствующего текстового поля
 
-        if(!email.isEmpty() && !password.isEmpty()) { // Проверка заполнения полей логина и пароля
+        if(!email.isEmpty() && !password.isEmpty()) { // Проверка полей логина и пароля на то, что они не пустые
 
-            SQLiteDatabase db = dbHelper.getReadableDatabase(); // Открытие базы данных в режиме только чтения
+            SQLiteDatabase db = dbHelper.getReadableDatabase(); // Открываем БД в режиме "только чтения"
 
-            String[] columns = {DatabaseHelper_Users_Merop.COLUMN_ID}; // Массив столбцов для выборки
-            String selection = DatabaseHelper_Users_Merop.COLUMN_EMAIL + "=? AND " + DatabaseHelper_Users_Merop.COLUMN_PASSWORD + "=?"; // Условие выборки
-            String[] selectionArgs = {email, password}; // Аргументы условия выборки
+            String[] columns = {DatabaseHelper_Users_Merop.COLUMN_ID}; // задаём массив столбцов, которые будут запрашиваться в базе данных
+            String selection = DatabaseHelper_Users_Merop.COLUMN_EMAIL + "=? AND " + DatabaseHelper_Users_Merop.COLUMN_PASSWORD + "=?"; // Определяем условия поиска в БД(ищем логин и парорь)
+            String[] selectionArgs = {email, password}; // Задаём массив аргументов для выборки записи, подходящих условию поиска
 
-            Cursor cursor = db.query(DatabaseHelper_Users_Merop.TABLE_U, columns, selection, selectionArgs, null, null, null); // выполнение выборки
+            Cursor cursor = db.query(DatabaseHelper_Users_Merop.TABLE_U, columns, selection, selectionArgs, null, null, null); // Выполняем запрос(выборку) к БД
 
             if(cursor != null && cursor.getCount() > 0) { // Проверка наличия найденных записей в таблице пользователей
-                Toast.makeText(this, "Вход выполнен", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Вход выполнен", Toast.LENGTH_SHORT).show(); // Вывод сообщения об удачном входе в аккаунт
                 Intent intent = new Intent(this, Main_Menu.class); // Переход на экран главного меню
                 startActivity(intent); // Запуск новой активности
-                finish(); // Закрываем текущую активити
-                Intent intent0 = new Intent(this, My_World.class); // Переход на экран личного кабинета
-                intent0.putExtra("email", emailEditText.getText().toString()); // Добавляет данные почты для экрана личного кабинета
+                finish(); // Закрываем текущую активности
+                Intent intent0 = new Intent(this, My_World.class); // Переход на экран ???
+                intent0.putExtra("email", emailEditText.getText().toString()); // Добавляет данные почты для экрана ???
                 startActivity(intent0); // Запуск новой активности My_World
             } else {
                 Toast.makeText(this, "Неверный логин или пароль", Toast.LENGTH_SHORT).show(); // Вывод сообщения об ошибке
@@ -78,7 +79,7 @@ public class Autor_Regis extends AppCompatActivity {
     // Закрываем базу данных при уничтожении активности
     @Override // Переопределение родительского метода
     protected void onDestroy() {
-        super.onDestroy(); // Закрываем активити
-        dbHelper.close(); // Закрытие соединения с базой данных для освобождения ресурсов и избежания потенциальных проблем с безопасностью.
+        super.onDestroy(); // Закрываем активности
+        dbHelper.close(); // Закрытие соединения с базой данных для освобождения ресурсов и избежания потенциальных проблем с безопасностью
     }
 }
